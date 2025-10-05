6d0b7c2a1d162efb820175fe1126af93f5c80178
PrakharShrma <prakharmrk50@gmail.com>
2025-10-05 15:35:17 +0530

    Final commit

--- Optimal Ship Routing/frontend/src/main.jsx
+++
@@ -1,8 +0,0 @@
-// main.jsx
-
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './global.css'; // NEW: Import a global CSS file
-
-createRoot(document.getElementById('root')).render(<App />);
